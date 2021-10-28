670fc559293d7962ea6b48c50ff3c5edb6d04236
dheanira <dheasucara@gmail.com>
2021-10-28 17:18:46 +0700

    css,required,icon

--- form_tambah.php
+++ form_tambah.php
@@ -1,42 +1,38 @@
 <html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <title>Data Mahasiswa</title>
 <body>
 
     <h1><center>TAMBAHKAN DATA</center></h1></br>
-
+    <div class="container">
     <form method="POST" action="tambah.php">
-    <table align="center" cellpadding="5" cellspacing="0">
-       <tr>
-           <td>NIM</td>
-           <td><input type="text" name="nim"></td>
-        </tr>
-        <tr>
-           <td>Nama Mahasiswa</td>
-           <td><input type="text" name="nama"></td>
-        </tr>
-        <tr>
-           <td>Tempat Lahir</td>
-           <td><input type="text" name="tempatlahir"></td>
-        </tr>
-        <tr>
-           <td>Tanggal Lahir</td>
-           <td><input type="date" name="tanggallahir"></td>
-        </tr>
-        <tr>
-           <td>Jurusan</td>
-           <td><input type="text" name="jurusan"></td>
-        </tr>
-        <tr>
-           <td>Fakultas</td>
-           <td><input type="text" name="fakultas"></td>
-        </tr>
-        <tr>
-           <td></td>
-           <td align="right" colspan="3"><input type="submit" name="" value="Simpan Data">
-        </tr> 
-    </table> 
-        <center><a href="index.php">Lihat Data Mahasiswa</a></center>
-    </form>
-
+    <form>
+      <div class="mb-3">
+         <label class="form-label">NIM</label>
+         <input type="text" class="form-control" id="exampleInputEmail1" name="nim" required >
+      </div>
+      <div class="mb-3">
+         <label class="form-label">Nama Mahasiswa</label>
+         <input type="text" class="form-control" id="exampleInputEmail1" name="nama" required >
+      </div>
+      <div class="mb-3">
+         <label class="form-label">Tempat Lahir</label>
+         <input type="text" class="form-control" id="exampleInputEmail1" name="tempatlahir" required >
+      </div>
+      <div class="mb-3">
+         <label class="form-label">Tanggal Lahir</label>
+         <input type="date" class="form-control" id="exampleInputEmail1" name="tanggallahir" required >
+      </div>
+      <div class="mb-3">
+         <label class="form-label">Jurusan</label>
+         <input type="text" class="form-control" id="exampleInputEmail1" name="jurusan" required >
+      </div>
+      <div class="mb-3">
+         <label class="form-label">Fakultas</label>
+         <input type="text" class="form-control" id="exampleInputEmail1" name="fakultas" required >
+      </div>     
+      <button type="submit" class="btn btn-primary" value="Simpan Data" name="" >Simpan Data</button>
+      <a class="btn btn-success" href="index.php">Lihat Data Mahasiswa</a>
+   </form>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,41 +1,46 @@
 <html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
 <title>Data Mahasiswa</title>
 <body>
-    <center>
-        <h1>DATA MAHASISWA</h1></br>
-
-            <table border="1" cellpadding="5" cellspacing="0">
+      
+        <div class="container">
+        <h1>DATA MAHASISWA</h1>
         <nav>
-        <a href="tambah.php">Tambah Data</a>
+        <a class="btn btn-success" href="tambah.php">Tambah Data</a>
         </nav>
-        <br>
-            <tr>
-                <th>NIM</th>
-                <th>NAMA</th>
-                <th>TEMPAT LAHIR</th>
-                <th>TANGGAL LAHIR</th>
-                <th>JURUSAN</th>
-                <th>FAKULTAS</th>
-            </tr>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NIM</th>
+      <th scope="col">NAMA</th>
+      <th scope="col">TEMPAT LAHIR</th>
+      <th scope="col">TANGGAL LAHIR</th>
+      <th scope="col">JURUSAN</th>
+      <th scope="col">FAKULTAS</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php include("koneksi.php");
+    if($conn){
+    $hasil=mysqli_query($conn,"SELECT*FROM mahasiswa");
+    while ($row = mysqli_fetch_array($hasil))
+    {
+        echo    " <tr><td>$row[nim]</td>
+                <td >$row[nama]</td>
+                <td >$row[tempatlahir]</td>
+                <td >$row[tanggallahir]</td>
+                <td >$row[jurusan]</td>
+                <td >$row[fakultas]</td>
+                <td > <a class='btn btn-primary' href='form_ubah.php?nim=$row[nim]'><i class='bi bi-pencil-square'></i></a></td>
+                <td ><a class='btn btn-danger' href='hapus.php?nim=$row[nim]'><i class='bi bi-trash'></i></a></td>
+                </tr>";
+        $row=mysqli_query($conn,"select*from mahasiswa where nim='$row[nim]'");
 
-<?php include("koneksi.php");
-if($conn){
-$hasil=mysqli_query($conn,"SELECT*FROM mahasiswa");
-while ($row = mysqli_fetch_array($hasil))
-{
-    echo    " <tr><td align=center>$row[nim]</td>
-            <td align=center>$row[nama]</td>
-            <td align=center>$row[tempatlahir]</td>
-            <td align=center>$row[tanggallahir]</td>
-            <td align=center>$row[jurusan]</td>
-            <td align=center>$row[fakultas]</td>
-            <td align=center><a href='form_ubah.php?nim=$row[nim]'>Ubah</a>
-            || <a href=hapus.php?nim=$row[nim]>Hapus</a>
-            </tr>";
-    $row=mysqli_query($conn,"select*from mahasiswa where nim='$row[nim]'");
-
-}
-}
-?>
-</table>
+    }
+    }
+    ?>
+  </tbody>
 </html>
