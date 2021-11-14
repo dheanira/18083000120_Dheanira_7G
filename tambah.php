e9fe3cd6852598d952b98777ee137d5a320811c4
dheanira <81346911+dheanira@users.noreply.github.com>
2021-11-14 17:12:28 +0700

    update1

--- tambah.php
+++ tambah.php
@@ -3,12 +3,16 @@ include("koneksi.php");
 
 $nim=$_POST['nim'];
 $nama=$_POST['nama'];
+$jenis=$_POST['jenis'];
+$alamat=$_POST['alamat'];
 $tempatlahir=$_POST['tempatlahir'];
 $tanggallahir=$_POST['tanggallahir'];
+$pend=implode(', ', $_POST['pend']);
 $jurusan=$_POST['jurusan'];
 $fakultas=$_POST['fakultas'];
 
-mysqli_query($conn,"insert into mahasiswa values('$nim','$nama','$tempatlahir','$tanggallahir','$jurusan','$fakultas')");
+mysqli_query($conn,"insert into mahasiswa values
+('$nim','$nama','$jenis','$alamat','$tempatlahir','$tanggallahir','$pend','$jurusan','$fakultas')");
 
 header("location:form_tambah.php");
 ?>
--- ubah.php
+++ ubah.php
@@ -3,13 +3,16 @@ include ("koneksi.php");
 
 $nim		 = $_POST['txtnim'];
 $nama		 = $_POST['txtnama'];
+$jenis		 = $_POST['txtjenis'];
+$alamat		 = $_POST['txtalamat'];
 $tempatlahir = $_POST['txttempatlahir'];
-$tanggallahir= $_POST['datetanggallahirr'];//// kurang r
+$tanggallahir= $_POST['datetanggallahirr'];
+$pend		 = implode(', ', $_POST['txtpend']);
 $jurusan	 = $_POST['txtjurusan'];
 $fakultas	 = $_POST['txtfakultas'];
 
-    $query="update mahasiswa set nama='$nama', tempatlahir='$tempatlahir', tanggallahir='$tanggallahir', 
-    jurusan='$jurusan', fakultas ='$fakultas' where nim='$nim'";
+    $query="update mahasiswa set nama='$nama', jenis='$jenis', alamat='$alamat', tempatlahir='$tempatlahir', tanggallahir='$tanggallahir', 
+    pend='$pend', jurusan='$jurusan', fakultas ='$fakultas' where nim='$nim'";
     $hasil=mysqli_query($conn,$query)or die(mysqli_error($conn));
 
 header("location:index.php");
